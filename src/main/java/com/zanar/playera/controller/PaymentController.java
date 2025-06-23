d8012a6015a4949c0ce7d59a87bca997344fb7ac
RAFA ATH ZANAR <zanarmhmra.21@uom.lk>
2025-06-23 22:45:53 +0530

    payment controller

--- src/main/java/com/zanar/playera/controller/PaymentController.java
+++ src/main/java/com/zanar/playera/controller/PaymentController.java
@@ -1,33 +1,32 @@
-//package com.zanar.playera.controller;
-//
-//import com.stripe.exception.StripeException;
-//import com.stripe.model.PaymentIntent;
-//import com.zanar.playera.request.PaymentRequest;
-//import com.zanar.playera.service.StripeService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/api/payment")
-//public class PaymentController {
-//
-//    @Autowired
-//    private StripeService stripeService;
-//
-//    @PostMapping("/create-payment-intent")
-//    public ResponseEntity<?> createPaymentIntent(@RequestBody PaymentRequest paymentRequest) {
-//        try {
-//            // Calculate amount in smallest unit (e.g., cents for USD)
-//            int amount = paymentRequest.getAmount() * 100;
-//
-//            // Create a PaymentIntent using StripeService
-//            PaymentIntent paymentIntent = stripeService.createPaymentIntent(amount, "lkr", paymentRequest.getDescription());
-//
-//            // Return the client secret of the payment intent (needed for frontend)
-//            return ResponseEntity.ok(paymentIntent.getClientSecret());
-//        } catch (StripeException e) {
-//            return ResponseEntity.badRequest().body("Error occurred: " + e.getMessage());
-//        }
-//    }
-//}
+package com.zanar.playera.controller;
+
+import com.zanar.playera.dto.PaymentRequestDTO;
+import com.zanar.playera.dto.PaymentResponseDTO;
+import com.zanar.playera.service.PaymentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/payments")
+public class PaymentController {
+  @Autowired
+  private PaymentService paymentService;
+
+  @GetMapping
+  public ResponseEntity<List<PaymentResponseDTO>> listPayments() {
+    return ResponseEntity.ok(paymentService.listPayments());
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<PaymentResponseDTO> getPaymentById(@PathVariable Long id) {
+    return ResponseEntity.ok(paymentService.getPaymentById(id));
+  }
+
+  @PostMapping
+  public ResponseEntity<PaymentResponseDTO> createPayment(@RequestBody PaymentRequestDTO dto) {
+    return ResponseEntity.ok(paymentService.createPayment(dto));
+  }
+}
